a4b692aa3791882ee1d9719b9339b557877fe306
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2019-07-19 21:18:26 +0300

    Update logs

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG } from "../actions/Types";
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG, SET_CURRENT, CLEAR_CURRENT, UPDATE_LOG } from "../actions/Types";
 
 const initialState = {
     logs: null,
@@ -22,5 +22,11 @@ export default (state = initialState, action) => {
             return { ...state, loading: false, logs: [...state.logs, action.payload] };
         case DELETE_LOG:
             return { ...state, loading: false, logs: state.logs.filter(log => log.id !== action.payload) };
+        case SET_CURRENT:
+            return { ...state, current: action.payload };
+        case CLEAR_CURRENT:
+            return { ...state, current: null };
+        case UPDATE_LOG:
+            return { ...state, logs: state.logs.map(log => (log.id === action.payload.id ? action.payload : log)) };
     }
 };
