cc43e20fa0a9395a383b2b749f49f67bc60cc425
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2019-07-20 16:13:54 +0300

    Add Technicians

--- src/reducers/techReducer.js
+++ src/reducers/techReducer.js
@@ -13,6 +13,7 @@ export default (state = initialState, action) => {
         case GET_TECHS:
             return { ...state, loading: false, techs: action.payload };
         case TECHS_ERROR:
+            console.log("ERROR ", action.payload);
             return { ...state, loading: false, error: action.payload };
         case ADD_TECH:
             return { ...state, loading: false, techs: [...state.techs, action.payload] };
